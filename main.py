b06d44ef96da878e21dc06b4b0fbd2eb6a3ea542
Hasan <H_B_A_J@hotmail.com>
2014-03-02 11:04:03 +0300

    YEL3AN THAL WAJH EL 3'ATHEEEEETH

---
+++ main.py
@@ -0,0 +1,20 @@
+import struct
+
+
+def 
+
+
+funSymbol = "c"
+def SEND ( msg ):
+    print (msg)
+    return 0
+
+def LoadImage ( filename ):
+    fn = bytearray(map ((lambda x: ord(x)), list(filename)))
+    SEND ( bytes([0x0]) + struct.pack ( "i", len(filename) ) + ar )
+
+def 
+
+
+    
+LoadImage ( "kofta.jpg" )
