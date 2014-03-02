68b3a0c8534aa2deda07d23a0a57eeb007286e13
Yusuf <ymusleh@qatar.cmu.edu>
2014-03-02 21:09:56 +0300

    Hash functions added

---
+++ hash.py
@@ -0,0 +1,20 @@
+
+
+def CreateHashTable ( tableSize ):
+	ret = []
+	for i in range(0, tableSize):
+		ret.append ( [] )
+	return ret
+
+def HTlookUp(hashTable, HashFun, ID):
+	index = HashFun ( obj )
+	for i in hashTable[index]:
+		if i.ID == ID:
+			return i
+
+def HTinstert(hashTable, HashFun, obj):
+	hashTable[HashFun(obj)].append(obj)
+
+
+
+
