b09f4d0a0b613ddcb3285b862f9ef88e32bff5c6
Hasan <H_B_A_J@hotmail.com>
2014-03-02 11:35:55 +0300

    my edit

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def LoadImage ( filename ):
     fn = bytearray(map ((lambda x: ord(x)), list(filename)))
     SEND ( bytes([0x0]) + struct.pack ( "i", len(filename) ) + ar )
 
-def 
+def LoadFuckYou ( )
+    kofta
 
 
     
