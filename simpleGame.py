5d4cb3c90f6aa900aad47c7123878526fa684e68
Hasan <H_B_A_J@hotmail.com>
2014-03-03 23:09:16 +0300

    Moved init call to FTSArcade.py

--- simpleGame.py
+++ simpleGame.py
@@ -1,8 +1,6 @@
-#Small testing game
+# Small testing game
 
 from FTSArcade import *
-
-FTSInit ( )
 
 pic1 = LoadImage ("C:\\Users\\Hasan\\Documents\\Visual Studio 2013\\Projects\RPG\\Map Importer\\arena.bmp")
 sprite1 = CreateSprite(pic1)
@@ -12,4 +10,3 @@ while True:
     fDeltaTime = 1.0 / 60.0
     MoveSprite ( sprite1, 5.0 * fDeltaTime )
     Sync ( )
-    
